8bc79620cd661e991b55ba61b4b7c56c972587ad
Tobias Wedhorn <tobias.wedhorn@gmx.de>
2023-01-11 14:52:32 +0100

    implemented contentful library

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,24 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Movie({movieData}) {
-  const SPACE_ID = "62lmtlm7lbg7";
-  const ACCESS_TOKEN = "QR3HFbCqEVnm8I_KfCCS26hgoKPvKUkO_zd2RJ3jG7Q";
-  const imageId=movieData.fields.pictures.sys.id;
-
-  const imageDataUrl = `https://cdn.contentful.com/spaces/${SPACE_ID}/assets/${imageId}?access_token=${ACCESS_TOKEN}`;
-
-  const [imageUrl, setImageUrl] = useState();
+  const imageUrl = movieData.fields.pictures.fields.file.url;
   const title=movieData.fields.title
-  const year=movieData.fields.year
-
-  useEffect(() => {
-
-    axios.get(imageDataUrl).then((response) => {
-      //saving Image URL
-      setImageUrl(response.data.fields.file.url);
-      console.log(movieData);
-    });
-  }, []);
 
   return (
     <div className="card-movie" style={{ backgroundImage: `url(${imageUrl})` }}>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { createClient } from "contentful"
 import Movie from "./Movie";
 
+
 function Movies() {
-  //fetch test
-
   const [moviesCont, setMoviesCont] = useState([]);
-
-  const SPACE_ID = "62lmtlm7lbg7";
-  const ACCESS_TOKEN = "QR3HFbCqEVnm8I_KfCCS26hgoKPvKUkO_zd2RJ3jG7Q";
-
-  //link will retrieve a list of all entries in the dedault environment of the specified space
-  const URI = `https://cdn.contentful.com/spaces/${SPACE_ID}/entries?access_token=${ACCESS_TOKEN}`;
+  const client = createClient({ space: "62lmtlm7lbg7", accessToken: "QR3HFbCqEVnm8I_KfCCS26hgoKPvKUkO_zd2RJ3jG7Q" })
 
   useEffect(() => {
-    axios.get(URI).then((response) => {
-      //saving array of movie information containinig objects
-      setMoviesCont(response.data.items);
-    });
-  }, []);
+    const getAllEntries = async () => {
+      try {
+        await client.getEntries().then((entries) => {
+          setMoviesCont(entries.items)
+        })
+      } catch (error) {
+        console.log(`Error fetching authors ${error}`);
+      }
+    };
+    getAllEntries()
+  }, [])
 
 
 
