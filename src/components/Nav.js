a8208e79d1f0d34795c0633af8fc01d5bbf2d246
sluggit <slugspeed1@gmail.com>
2023-01-18 16:37:04 +0100

    m

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
+import SearchBar from './SearchBar'
 
 
 
 
-function Nav() {
+function Nav({setSearchResults}) {
   return (
       <div className='container-nav'>
           <div className='logo'>LOGO</div>
-          <div className='container-menu'>  <NavLink to='/'>Home</NavLink>  </div>
+      <div className='container-menu'>  <NavLink to='/'>Home</NavLink> <SearchBar setSearchResults={setSearchResults}/> </div>
+      
+
+
     </div>
   )
 }
--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Movie from "../components/MovieCard";
 import Movies from "./LandingPage";
-
+import Header from "../components/Header"
 function ResultPage({searchResults}) {
   return (
     <div className="container-app">
+     
       <div className="container-movie">
         <div className="wrapper-movie">
+        <Header/>
           {searchResults ? (
             searchResults.map((movieData, i) => {
               return <Movie movieData={movieData} key={i} />;
